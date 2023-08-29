c124555b3b2033e6934d3a8d29a765d80de965fc
Alberto <ajgf1984@gmail.com>
2023-08-29 15:29:07 +0100

    Bullets moving

--- bullet.js
+++ bullet.js
@@ -1,7 +1,8 @@
 function Bullet(x, y, parent) {
-
+    var self = this;
     this.x = x;
     this.y = y;
+    this.speed = 50;
     this.sprite;
 
     this.insertBullet = function () {
@@ -11,6 +12,26 @@ function Bullet(x, y, parent) {
         newBullet.style.top = this.y + 'px';
         this.sprite = newBullet;
         parent.appendChild(this.sprite);
+    }
+
+
+    this.move = function () {
+        // self.checkCollision();
+        // Al tener la referencia la parte superior del tablero, habría que 
+        // restar para que las balas fuesen hacia arriba.
+        self.y = self.y - self.speed;
+        self.sprite.style.top = self.y + 'px';
+        // Si alcanza el borde o un poco más:
+        if (self.y <= 0) {
+            self.removeBullet();
+        }
+    }
+
+    this.timerId = setInterval(this.move, 100);
+
+    this.removeBullet = function () {
+        parent.removeChild(this.sprite);
+        clearInterval(this.timerId);
     }
 }
 
